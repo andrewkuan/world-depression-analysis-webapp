01a7b9b26a61160ad52d6cce0edf57bf8ae32229
andrewkuan <andrewkuan10@gmail.com>
2020-04-12 17:44:30 +0800

    firebase added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'world-depression-analysis-webapp';
+  title = 'Depression 2020s';
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyD9GaJf3D4TFfuGqQkDwKgZV-XNIg8Z9GM",
+    authDomain: "depression-2020s.firebaseapp.com",
+    databaseURL: "https://depression-2020s.firebaseio.com",
+    projectId: "depression-2020s",
+    storageBucket: "depression-2020s.appspot.com",
+    messagingSenderId: "103461173208",
+    appId: "1:103461173208:web:fe69f9b022e8c0d0e324ad",
+    measurementId: "G-DMT444YJM4"
+  }
 };
 
 /*
