85d96cffbe584963828282e194de7d1def4c0f0d
andrewkuan <andrewkuan10@gmail.com>
2020-04-25 20:16:54 +0800

    added new admin pages and services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UnauthComponent } from './components/public/unauth/unauth.component';
-import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
-import { AdminloginComponent } from './components/public/adminlogin/adminlogin.component';
 import { DonateinfoComponent } from './components/public/donateinfo/donateinfo.component';
 import { AboutinfoComponent } from './components/public/aboutinfo/aboutinfo.component';
 import { BlogComponent } from './components/public/blog/blog.component';
 import { HomeinfoComponent } from './components/public/homeinfo/homeinfo.component';
+
+
+import { AdminloginComponent } from './components/public/adminlogin/adminlogin.component';
+
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { EbookcrudComponent } from './components/admin/ebookcrud/ebookcrud.component';
+import { CarouselcrudComponent } from './components/admin/carouselcrud/carouselcrud.component';
+import { BlogcrudComponent } from './components/admin/blogcrud/blogcrud.component';
 
 import { AuthGuardService } from './services/auth/auth-guard.service';
 
@@ -19,9 +25,14 @@ const routes: Routes = [
   { path: 'blog', component : BlogComponent},
   { path: 'about', component : AboutinfoComponent},
   { path: 'donate', component : DonateinfoComponent},
+  { path: 'unauth', component : UnauthComponent},
+  
   { path: 'adminlogin', component : AdminloginComponent, canActivate: [InnerPageGuardService]},
+
   { path: 'dashboard', component : DashboardComponent, canActivate: [AuthGuardService]},
-  { path: 'unauth', component : UnauthComponent}
+  { path: 'blogcrud', component : BlogcrudComponent, canActivate: [AuthGuardService]},
+  { path: 'carouselcrud', component : CarouselcrudComponent, canActivate: [AuthGuardService]},
+  { path: 'ebookcrud', component : EbookcrudComponent, canActivate: [AuthGuardService]},
 ];
 
 @NgModule({
